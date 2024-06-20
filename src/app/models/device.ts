fcec211ba616d8d204ebb4f95bcf441c55545271
Daniel Eichhorn <dani.eichhorn@squix.ch>
2024-06-20 13:59:14 +0200

    Improve styling

--- src/app/models/device.ts
+++ src/app/models/device.ts
@@ -2,6 +2,8 @@ export interface Device {
     id: string;
     name: string;
     manufacturer: string;
+    mcuFamily: string;
+    tags: string[];
     imageThumbnail: string;
     image: string;
     productLink: string;
